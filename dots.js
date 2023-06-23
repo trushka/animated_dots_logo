f63596362936f1d29e167cd2ac0214f4f4abb01a
trushka <trueshko@gmail.com>
2023-06-23 22:36:23 +0300

    Rendering using canvas

--- dots.js
+++ dots.js
@@ -1,8 +1,10 @@
 const $win=$(window),
 			$el=$('.dashes'),
-			nx=$el.css('--w'),
 			ny=$el.css('--h'),
-			n=nx*ny, dots=[];
+			nx=$el.css('--w')||ny/$el.height()*$el.width(),
+			n=nx*ny, dots=[],
+
+			ctx=$el[0].getContext('2d');
 
 function vec2(x=0, y){
 	if (!new.target) return new vec2(x, y);
@@ -12,8 +14,8 @@ function vec2(x=0, y){
 vec2.prototype = {
 	mult: function(val) {
 		val=val.xy||[val, val];
-		this.xy[0] *= val[0]||val
-		this.xy[1] *= val[1]||val
+		this.xy[0] *= val[0];
+		this.xy[1] *= val[1];
 		if (val[0]==val[1] && '_l' in this) {
 			this._l *= val[0];
 			this._l2 = this._l * this._l;
@@ -38,10 +40,11 @@ vec2.prototype = {
 		return this._l ?? (this._l = Math.sqrt(this.l2))
 	},
 	set l(length) {
-		this.mult(length/this.l)
+		this.setL(length)
 	},
 	setL: function(length) {
-		return this.mult(length/this.l)
+		if (+length) length /= this.l
+		return this.mult(length)
 	},
 	upd: function() {
 		delete this._l;
@@ -61,42 +64,63 @@ for (let iy=0; iy<ny; iy++) {
 		x = ix + .5;
 		y = iy + .5;
 		dots.push({pos0: vec2(x, y), pos: vec2(x, y), v: vec2()})
-		$('<div />').css('transform', `translate(${x}em, ${y}em)`).appendTo($el)
+		//$('<div />').css('transform', `translate(${x}em, ${y}em)`).appendTo($el)
 	}
 };
 const $dot=$el.children();
-let touch;
+
+let touch, box, scale;
 $win.on('mousemove touchstart touchmove', function setTarg(e){
 	if (!e.touches) {
-		const {left, top, width, height} = $el[0].getBoundingClientRect();
+		const {left, top, width, height} = box;
 		touch=vec2(
 			(e.clientX - left)/width*nx,
 			(e.clientY - top)/height*ny
 		)
 		//console.log(touch.xy)
 	} else setTarg(e.changedTouches[0]);
-})
+})	
+.resize(e=>{
+ 	box = $el[0].getBoundingClientRect();
+ 	$el[0].width = box.width*devicePixelRatio;
+ 	$el[0].height= box.height*devicePixelRatio;
+ 	ctx.setTransform(1, 0, 0, 1, 0, 0);
+ 	const sc = $el[0].height / ny;
+ 	ctx.scale(sc, sc)
+	ctx.strokeStyle='#fffa';
+	ctx.lineCap = "round";
+	ctx.lineWidth=.7
+}).resize();
+
 let t0=0;
 requestAnimationFrame(function anim(t){
 	const dt = Math.min(t-t0, 10);
 	t0=t;
 	requestAnimationFrame(anim);
-	if (!touch) return;
+	//if (!touch) return;
+	ctx.clearRect(0,0, nx, ny);
 
 	for (let i=0; i<n; i++) {
 		let {pos0, pos, v} = dots[i];
 		const
-			dTouch = vec2(pos).sub(touch),
+			dTouch = vec2(pos).sub(touch||vec2()),
 
 			dPos = vec2(pos0).sub(pos),
 
-			f=dTouch.setL(5280/(dTouch.l2**4)).add(dPos.mult(.005));//
+			f=touch ? dTouch.setL(5280/(dTouch.l2**4)) : vec2();
 
-		v.add(f.mult(dt*.5)).setL(Math.min(v.l, .3)).mult(1/(1+dt*.05));
+		f.add(dPos.mult(.005));//
+
+		v.add(f.mult(dt*.1)).setL(Math.min(v.l, .3)).mult(Math.exp(-dt*.02));
 
 		const [x, y] = pos.add(vec2(v).mult(dt)).xy;
 		
-		$dot[i].style.transform=`translate(${x}em, ${y}em)`;
+	    //ctx.fillStyle = ctx.strokeStyle = color;
+			ctx.beginPath()
+			ctx.moveTo(x, y);
+			ctx.lineTo(x, y);
+			//ctx.closePath();
+			ctx.stroke();
 
 	}
 	//targets.length = 0;
